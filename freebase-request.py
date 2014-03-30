faebc485f332e89ce29331b0ddbec6429b79cd54
Raphaël Drion <raphael.drion@gmail.com>
2014-03-30 11:29:32 +0200

    Update freebase request

--- freebase-request.py
+++ freebase-request.py
@@ -1,24 +1,22 @@
 import urllib
 import simplejson as json
+from twython import Twython
 
+#twitter initialisation
 
+API_KEY = 'BoP0mE4gtGZlh1me29lug'
+API_SECRET = 'xteLnU2uu3KEKxZx8hc1wfPic0gs5JvosmRP9VBA6c'
+ACCESS_TOKEN = '2417467056-2aZr2bQSGaDcRnbqR7QAHC95TXXSPbyQr2jONYa'
+ACCESS_TOKEN_SECRET = 'LBKmR3OXxVFiLEM9zeiYNe0tKwUyiFdfmz27ts36AuhHW'
 
+twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+#freebase polics retrieval
 
 apiKey = 'AIzaSyD-mnJ5Gff-xJDpEU78Pc5iXLbMHmK4FPA'
 url = 'https://www.googleapis.com/freebase/v1/search?'
 
-party = 'democratic'
 party = 'republican'
-
-
-# parameters = { 
-#     'filter': '(all type:/government/politician member_of:' + party + ')',
-#     'limit': 200,
-#     'indent': True,
-#     'spell': 'always',
-#     'key': apiKey,
-#     'output' : '(/internet/social_network_user/twitter_id)'
-#   };
 
 parameters = { 
     'filter': '(all type:politician member_of:' + party + ')',
@@ -28,8 +26,6 @@ parameters = {
     'key': apiKey,
     'output' : '(/internet/social_network_user/twitter_id)'
   };
-
-
 
 parameters = urllib.urlencode(parameters)
 
@@ -45,7 +41,7 @@ count = 0
 for item in content:
 	x = item['output']['/internet/social_network_user/twitter_id']
 	if x != {}:
-   		print x
-   		count = count + 1
+            y = x['/internet/social_network_user/twitter_id'][0]
+            print y
 
 print count
